bf43d0cd24e68d855140199531392f8afffad4b4
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-25 14:19:17 +0530

    Added the Test Class and Sports Class || Main is to be added

--- res_class.cpp
+++ res_class.cpp
@@ -12,3 +12,23 @@ class Student{
         name = n;
     }
 };
+class Sports{
+    protected:
+    int score;
+
+    public:
+    Sports(int sc = 0){
+        score = sc;
+    }
+};
+
+class Test:public Student{
+    protected:
+    int m1, m2;
+
+    public:
+    Test(int mr1 = 0, int mr2 = 0, string nm = "", int rN = 1):Student(nm, rN){
+        m1 = mr1;
+        m2 = mr2;
+    }
+};
